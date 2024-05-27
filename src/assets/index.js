ca91940f70317fb65e0ae55e2e2e26194118406d
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-27 18:30:51 +0530

    added logo ,scarch bar,card and account

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import logo1 from "./images/logo1.png";
+import logo2 from "./images/logo2.png";
+
+
+
+export{
+    logo1,
+    logo2
+
+};
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens:{
+        xs:"320px",
+        sm:"375px",
+        sml:"500px",
+        md:"667px",
+        mdl:"768px",
+        lg:"960px",
+        lgl:"1024px",
+        xl:"1280px"
+      },
+
+      fontFamily:{
+        bodyFont: ["Poppins", "sans-serif"],
+         titleFont: ["Montserrat", "sans-serif"],
+      },
+        colors:{
+          bodyColor:"#2eb82e",
+          lightText:"#007bff",
+          boxBg: "linear-gradient(145deg, #01e202, #23272b)",
+          designColor:"#ff014f",
+        }
+    },
+  },
+  plugins: [],
+}
