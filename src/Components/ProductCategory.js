6d381068e6ef8cc96d5503609d69c4b525c7fd1b
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-31 09:29:08 +0530

    addes mobile logo and creted logo array

--- src/Components/ProductCategory.js
+++ src/Components/ProductCategory.js
@@ -1,6 +1,32 @@
 import React from 'react'
 
+
 export const ProductCategory = () => {
+ const productLogo = [
+    {
+        id: 1, logo: 'https://w7.pngwing.com/pngs/664/673/png-transparent-apple-logo-iphone-computer-apple-logo-company-heart-logo.png'
+    },
+    {
+        id: 2, logo: 'https://toppng.com/uploads/preview/samsung-logo-png-transparent-images-11661248818w9r5qkhfaz.png'
+    },
+    {
+        id: 3, logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkhqjL9P_uxJb0gq6vBUeR6XPMjAQZESiKAQ&s'
+    },
+    {
+        id: 4, logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHnur-JHzlVPZhVcBvmaRcQcmKely-Gzt11Q&s'
+    },
+    {
+        id: 5, logo: 'https://w7.pngwing.com/pngs/344/344/png-transparent-google-logo-google-logo-g-suite-google-text-logo-symbol-thumbnail.png'
+    },
+    {
+        id: 6, logo: 'https://e7.pngegg.com/pngimages/234/461/png-clipart-xiaomi-mi-1-graphics-logo-xiaomi-logo-text-trademark-thumbnail.png'
+    },
+    {
+        id: 7, logo: 'https://e7.pngegg.com/pngimages/854/175/png-clipart-oppo-logo-phone-identity.png'
+    }
+
+ ];
+
   return (
     <div>ProductCategory</div>
   )
