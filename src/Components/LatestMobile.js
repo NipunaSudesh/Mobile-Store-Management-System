9305443cb13ebf3820173ed43f9919a1e3b87612
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-02 08:52:39 +0530

    updated style

--- src/Components/LatestMobile.js
+++ src/Components/LatestMobile.js
@@ -6,10 +6,10 @@ import {apple13} from '../assets/index';
 export const LatestMobile = () => {
   return (
     <div className='flex flex-col'>
-        <div>
+        <div className='mx-auto'>
             <Header Title='LATEST MOBILE PHONES' />
         </div>
-        <div className='grid md:grid-cols-3 mdl:grip-cols-2 lgl:grid-cols-5 gap-4'>
+        <div className='grid md:grid-cols-3 mdl:grip-cols-2 lgl:grid-cols-5 gap-x-4 '>
           <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
           price={"RS.350000"}
           image={apple13}/>
