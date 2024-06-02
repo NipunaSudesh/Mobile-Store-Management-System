d2ea6d0b4aecb4259ce1b32ebcd4fffbafd633cd
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-02 15:08:02 +0530

    added header

--- src/Components/FeaturedMobile.js
+++ src/Components/FeaturedMobile.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from './Header'
 
 export const FeaturedMobile = () => {
   return (
-    <div>FeaturedMobile</div>
+    <div>
+        <Header Title='Featured Mobile'/>
+    </div>
   )
 }
