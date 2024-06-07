3cd2f5e3281444a38e60dbbd796f55b7ee8ccd41
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-07 09:07:56 +0530

    updated

--- src/Components/FeaturedMobile.js
+++ src/Components/FeaturedMobile.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { Header } from './Header';
 import { ProductCard } from './ProductCard';
-import {apple13} from '../assets/index';
+import { apple13 } from '../assets/index';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -11,47 +11,65 @@ import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 
 export const FeaturedMobile = () => {
   return (
-    <div className="lgl:max-w-[1300px] w-full  mx-auto px-4 relative">
-    <Swiper
-      modules={[Pagination, Navigation, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      navigation
-      autoplay={{ delay: 3000 }}
-      loop
-    >
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-     <SwiperSlide> 
-     <ProductCard name={"Apple iPhone 13 Pro Max 256GB"}
-          price={"RS.350000"}
-          image={apple13}/>
-     </SwiperSlide>
-    </Swiper>
-  </div>
-  )
-}
+    <div className="flex flex-col gap-5">
+      <div>
+        <Header Title="Featured Mobile" />
+      </div>
+      <div className="lgl:max-w-[1300px] w-full mx-auto px-4 relative">
+        <Swiper
+          modules={[Pagination, Navigation, Autoplay]}
+          spaceBetween={10}
+          slidesPerView={5}
+          slidesPerColumn={2} 
+          pagination={{ clickable: true }}
+          navigation
+          autoplay={{ delay: 3000 }}
+          loop
+        >
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard
+              name={"Apple iPhone 13 Pro Max 256GB"}
+              price={"RS.350000"}
+              image={apple13}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
