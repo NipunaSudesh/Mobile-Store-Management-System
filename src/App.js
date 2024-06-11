6739762be641193d0a92df4806e8220fc9109d14
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-11 08:14:57 +0530

    added Routes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import {Featurchs} from './Components/Featurchs';
 import {FeaturedMobile} from './Components/FeaturedMobile';
 import {Contact} from './Components/Contact';
 import { Footer } from './Components/Footer ';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { SignUp } from './Pages/SignUp';
 
-
-function App() {
+function Home() {
   return (
 <div className='flex  flex-col gap-5 items-center '>
   <NavBar />
@@ -22,6 +23,20 @@ function App() {
   <Contact />
   <Footer />
 </div>
+
+  );
+}
+
+
+function App(){
+  return(
+    <Router>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/signup' element={<SignUp />} />
+  </Routes>
+</Router>
+
   );
 }
 
