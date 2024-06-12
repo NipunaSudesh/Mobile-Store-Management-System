e8d49e5f6ab5d2f67265feb345e6d0568d27c6ac
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-12 16:10:40 +0530

    connect mongodb

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -12,3 +12,18 @@ const port=5000;
 app.listen(port,()=>{
     console.log("Server is up and runing on port "+port);
 });
+
+const url ="mongodb+srv://nipuna:pass123@cluster0.lvhm2yt.mongodb.net/smartMobile?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(url,
+    {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    }
+);
+
+const connection =mongoose.connection
+
+connection.once("open",()=>{
+    console.log("mongoDB Connected!")
+});
