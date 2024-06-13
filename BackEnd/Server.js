d460202b8b9b52d7b331c4a6641ee8842fa0aa82
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-13 16:40:50 +0530

    fixed error

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-require('./db/mongoose');
-const userRoute=require('./models/User');
-const bodyParser = require('body-parser');
+require('./db/mongoose'); 
+const userRoute = require('./routes/User');
 const cors = require('cors');
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+
+app.use(express.json()); 
 app.use(cors());
-app.use(userRoute)
-const port=5000;
 
-app.listen(port,()=>{
-    console.log("Server is up and runing on port "+port);
+
+app.use('/api', userRoute);
+
+
+app.listen(port, () => {
+    console.log("Server is up and running on port " + port);
 });
-
--- BackEnd/db/mongoose.js
+++ BackEnd/db/mongoose.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const url ="mongodb+srv://nipuna:pass123@cluster0.lvhm2yt.mongodb.net/smartMobile?retryWrites=true&w=majority&appName=Cluster0";
+  const url ="mongodb+srv://nipuna:pass123@cluster0.lvhm2yt.mongodb.net/smartMobile?retryWrites=true&w=majority&appName=Cluster0";
+
+
 
 mongoose.connect(url,
     {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
+         useNewUrlParser: true,
+        useUnifiedTopology: true,
     }
 );
 
 const connection =mongoose.connection
+mongoose.set('strictQuery',true);
 
 connection.once("open",()=>{
     console.log("mongoDB Connected!")
