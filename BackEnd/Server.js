ff9c4e72c2e9f016ebe2a772f21773e2078fc99b
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-13 07:56:45 +0530

    start create user model

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+require('./db/mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
@@ -13,17 +13,3 @@ app.listen(port,()=>{
     console.log("Server is up and runing on port "+port);
 });
 
-const url ="mongodb+srv://nipuna:pass123@cluster0.lvhm2yt.mongodb.net/smartMobile?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose.connect(url,
-    {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-    }
-);
-
-const connection =mongoose.connection
-
-connection.once("open",()=>{
-    console.log("mongoDB Connected!")
-});
