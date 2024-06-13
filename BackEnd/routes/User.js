24da7193f28a4e7e040b5e1474d9e6bfb2bc449c
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-13 15:02:58 +0530

    created router

---
+++ BackEnd/routes/User.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const User =require("../models/User");
+
+router.post("/users",async (req,res) => {
+    const user =new User({
+        name :"test",
+        email:"text@gmail.com",
+        password:"123"
+    })
+    try{
+    await user.save();
+    res.status(201).send(user)
+    }catch(error){
+        res.status(400).send(error)
+    }
+});
