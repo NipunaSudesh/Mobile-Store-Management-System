fae76efba757d8b20be3bdab836a2139205e8d76
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-14 18:52:17 +0530

    create form frontend

--- FrontEnd/src/Pages/AddProduct.js
+++ FrontEnd/src/Pages/AddProduct.js
@@ -9,21 +9,21 @@ export const AddProduct = () => {
     const [brand, setBrand] = useState('');
     const [description, setDescription] = useState('');
     const [message, setMessage] = useState('');
-    const [navigate] =useNavigate('')
+    const navigate = useNavigate();
  
-    const handleCancel =()=>{
+    const handleCancel = () => {
         navigate('/');
-    }
+      };
 
   return (
     <div className='flex h-screen items-center justify-center'>
-    <div className=' flex  h-auto w-[500px] flex-col items-center justify-center  py-2 rounded shadow-2xl border-2'>
+    <div className=' flex  h-auto w-[700px] flex-col items-center justify-center  py-2 rounded shadow-2xl border-2'>
         <h1 className='text-center py-2 bg-green-400 w-full text-2xl mb-4'>Add Product</h1>
-        <form>
+        <form className='w-[80%] text-center'>
             <div className='mb-4'>
-                <lable className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Product Name</lable>
+                <lable className='block text-sm font-bold mb-2' htmlFor="password">Product Name</lable>
                 <input 
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                className=' shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                 id='name'
                 value={name}
                 type='text'
@@ -92,10 +92,10 @@ export const AddProduct = () => {
          
                 />
             </div>
-            <div className='flex gap-10 items-center justify-center'>
+            <div className='mb-4 flex gap-10 items-center justify-center'>
             <div className="flex items-center justify-between">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-green-500 hover:bg--green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Add Product
