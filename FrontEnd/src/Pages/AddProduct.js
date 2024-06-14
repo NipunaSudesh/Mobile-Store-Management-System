c2bc9b4f7358541a5c5d1204c6706f5035c90f71
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-14 18:21:01 +0530

    add header

--- FrontEnd/src/Pages/AddProduct.js
+++ FrontEnd/src/Pages/AddProduct.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const AddProduct = () => {
   return (
-    <div>AddProduct</div>
+    <div className='flex h-screen items-center justify-center'>
+    <div className=' flex h-auto w-[500px] flex-col items-center justify-center  py-2 rounded shadow-2xl border-2'>
+        <h1 className='text-center text-2xl mb-4'>Add Product</h1>
+    </div>
+    </div>
   )
 }
