647d9c61adeea90c84151d37f9a87323c31e4552
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-14 09:06:56 +0530

    fixed postman error

---
+++ BackEnd/index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const bodyPaeser=require("body-parser");
+require('./db/mongoos');
+const cors=require("cors");
+const userRouter =require('./Routes/User');
+
+const app =express();
+
+app.use(cors());
+app.use(bodyPaeser.json());
+app.use(userRouter);
+const port=5000
+
+app.listen(port,()=>{
+    console.log("server is up and running on port "+port)
+});
+
+
--- BackEnd/routes/User.js
+++ BackEnd/routes/User.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
+const express =require("express");
+const router =express.Router();
+const User =require("../model/user");
 
 
-router.post("/users", async (req, res) => {
-    const user = new User(req.body);
 
-    try {
-        await user.save();
-        res.status(201).send(user);
-    } catch (error) {
-        res.status(400).send({ error: error.message });
+router.post("/users",async (req,res)=>{
+    const user =new User(req.body);
+    console.log(req.body);
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(error){
+        res.status(400).send(error)
     }
 });
 
-module.exports = router;
+
+
+
+module.exports =router;
