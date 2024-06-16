e12f7bd3b6604902aa3b051fcd9796dc8e6f7599
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-16 11:26:25 +0530

    bcrypt user password

--- FrontEnd/src/Pages/AddProduct.js
+++ FrontEnd/src/Pages/AddProduct.js
@@ -95,7 +95,7 @@ export const AddProduct = () => {
             <div className='mb-4 flex gap-10 items-center justify-center'>
             <div className="flex items-center justify-between">
           <button
-            className="bg-green-500 hover:bg--green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Add Product
