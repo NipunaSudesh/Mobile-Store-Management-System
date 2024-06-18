5b272be9d99ecde315bd740f5124595584c4d0e5
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-18 13:36:08 +0530

    a

--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -1,24 +1,25 @@
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 
-const auth =async (req,res,next) =>{
-    try {
-        const token =req.header("Authorization").replace("Bearer","");
-        const decoded =jwt.verify(token,"mysecret");
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "mysecret");
 
-        const user = await User.findOne({
-            _id:decoded._id,
-            "tokens.token":token
-        });
-        if(!user){
-            throw new Error();
-        }
-
-        req.user =user;
-        next()
-    } catch (error) {
-        res.status(401).send({error:"please Auth"});
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token
+    });
+    
+    if (!user) {
+      throw new Error();
     }
+
+    req.user = user;
+    req.token = token; 
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate." });
+  }
 };
 
 module.exports = auth;
