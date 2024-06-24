5c43d7326f6811e3c1c42690c746a3e91e2686fd
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-24 18:41:54 +0530

    fixed register

--- BackEnd/routes/User.js
+++ BackEnd/routes/User.js
@@ -6,18 +6,17 @@ const auth = require("../middleware/auth");
 
 
 router.post("/user/register", async (req,res)=>{
-    const user =new User(req.body);
+    const { email, name, password } = req.body;
 
     try{
-        const dbUser =await user.findByCredentials(user.email);
+        const dbUser = await User.findOne({ email });
         if(dbUser){
             return res.status(400).json({ message: 'User already exists' });
         }
-        else{
+        const user = new User({ email, name, password });
         await user.save()
         //const token = await user.generateAuthToken();
         res.status(201).send(user)
-        }
     }catch(error){
         console.error('Error during registration:', error);
         res.status(500).send({error:error.massage || 'Server error' })
