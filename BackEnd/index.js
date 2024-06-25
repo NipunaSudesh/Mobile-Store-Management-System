dddeb209c1ff0ab6182cded3a5e08fbca80a1c4b
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-25 08:28:57 +0530

    created app product router

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,15 +1,17 @@
 const express=require("express");
-const bodyPaeser=require("body-parser");
+const bodyParser = require("body-parser");
 require('./db/mongoos');
 const cors=require("cors");
 const userRouter =require('./Routes/User');
+const productRouter =require('./Routes/Product');
 
 const app =express();
 app.use(express.json());
 app.use(cors());
-app.use(bodyPaeser.json());
+app.use(bodyParser.json()); 
 app.use(userRouter);
-const port=5000
+app.use(productRouter);
+const port=5000;
 
 app.listen(port,()=>{
     console.log("server is up and running on port "+port)
