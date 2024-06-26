2d4da2cb18f2108ff665132abfcb6144a73ade95
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-26 17:59:44 +0530

    created FeaturedMobile model

---
+++ BackEnd/model/featuredmobile.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const featuredmobileSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        trim: true
+    },
+    price:{
+        type:Number,
+        require:true,
+        min: 0
+    },
+    year:{
+        type:Number,
+        require:true,
+        min: 1990,
+    },
+    brand:{
+        type:String,
+        require:true,
+        trim: true     
+    },
+    details:{
+        type:String,
+        require:true,
+        trim: true 
+    },
+    imgURL:{
+        type:String,
+        require:true,
+        validate: {
+            validator: function(v) {
+                return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }   
+    },
+    description:{
+        type:String,
+        require:true ,
+        trim: true
+    }
+})
+
+const FeaturedMobile = mongoose.model("FeaturedMobile", featuredmobileSchema);
+module.exports = FeaturedMobile;
