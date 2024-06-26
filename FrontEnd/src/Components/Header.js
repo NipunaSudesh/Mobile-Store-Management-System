7335dcc9cfaa8aacf64904bbe09277b91b07599f
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-26 17:47:12 +0530

    updated style in header

--- FrontEnd/src/Components/Header.js
+++ FrontEnd/src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = ({Title}) => {
   return (
-    <div className='mt-10 mb-5'>
+    <div className='mt-10 mb-5 w-full border-b-2 border-gray-500'>
         <h2 className=' text-4xl font-semibold'>{Title}</h2>
     </div>
   )
