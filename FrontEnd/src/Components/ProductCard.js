8af43d292f590ea5485d4bba4dc836c10ea50523
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-06-30 08:56:03 +0530

    get mobile form data base

--- FrontEnd/src/Components/ProductCard.js
+++ FrontEnd/src/Components/ProductCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 
-export const ProductCard = ({name,deltails,price,image}) => {
-  const discountedPrice = price * 1.05;
+export const ProductCard = ({name,details,price,image}) => {
+  const discountedWithOutPrice = price * 1.05;
   return (
     <div className='h-auto w-full items-center rounded-5g shadow-lg flex flex-col transition-colors duration-1000 rounded-[5%] my-10 group hover:bg-gradient-to-b
     hover:from-gray-900 hover:gray-900'>
@@ -33,11 +33,11 @@ export const ProductCard = ({name,deltails,price,image}) => {
 </div>
 
       <div className="flex flex-col mb-2 mt-2 items-center text-center">
-        <h2 className='font-semibold'>{name}</h2>
-        <h2 className='font-normal'>{deltails}</h2>
+        <h2 className='font-semibold text-blue-500'>{name}</h2>
+        <h2 className='font-normal'>{details}</h2>
         <div className='flex gap-4'>
-            <h3><span className='text-red-600 line-through'>RS.{discountedPrice}</span></h3>
-            <h3><span className='text-blue-600'>RS.{price}</span></h3>
+            <h3><span className=' line-through text-gray-400'>RS.{discountedWithOutPrice}</span></h3>
+            <h3><span className='text-red-500'>RS.{price}</span></h3>
         </div>
 
       </div>
