1879b96cebe8749a45c02e34b78316d1d5a1f652
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-07-07 19:24:17 +0530

    created updated

--- FrontEnd/src/Pages/adminPanel/EditLatestMobile.js
+++ FrontEnd/src/Pages/adminPanel/EditLatestMobile.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 
-export const EditMobile = () => {
+export const EditLatestMobile = () => {
     const [mobile, setMobile] = useState({
         name: '',
         price: '',
