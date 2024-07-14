9f48d2e7c65db9cbcd03cf567937709e58843612
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-07-14 18:10:28 +0530

    created card module

---
+++ BackEnd/model/card.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const CardSchema =new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user',
+        require:true
+    },
+    productId: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, default: 1 },
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
