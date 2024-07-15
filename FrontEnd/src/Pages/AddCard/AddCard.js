2e26faca8450a69f29731cfcac6d7f235b3854dd
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-07-15 14:16:33 +0530

    create card frontend

--- FrontEnd/src/Pages/AddCard/AddCard.js
+++ FrontEnd/src/Pages/AddCard/AddCard.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {TNavBar} from '../../Components/navbar/TNavBar';
+import {Cart} from './Cart';
 
 export const AddCard = () => {
   return (
-    <div>AddCard</div>
+    <div className='flex flex-col gap-4 w-full'>
+      <div>
+          <TNavBar />
+      </div>
+      <div className='flex gap-4 w-full mx-10'>
+        <div className='w-3/5 flex flex-col gap-2  '>
+        <h1 className='text-xl font-semibold'>Shopping Cart</h1>
+        <div className='mx-2'>
+        <h2 className='text-sm font font-semibold'>Shopping by Smart Mobile</h2>
+      </div>
+        <div className='flex flex-col gap-2'>
+          <Cart />
+          <Cart />
+          <Cart />
+        </div>
+        </div>
+
+        <div className='w-2/5 flex flex-col gap-2  bg-slate-200'>
+        <h1 className='text-xl font-semibold'>Summary</h1>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ FrontEnd/src/Pages/AddCard/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { apple13 } from '../../assets';
+import { MdDeleteForever } from "react-icons/md";
+
+export const Cart = () => {
+  return (
+    <div className='bg-gray-200 w-full h-[100px] flex  text-start shadow-lg rounded-lg'>
+        <div className='flex gap-2 w-full'>
+          <div className='w-1/5 bg-red-200 m-2'>
+          <img className='object-cover' src={apple13} alt='' />
+          </div>
+          <div className='m-2 flex flex-col w-3/5'>
+            <h1 className='text-xl font-semibold'>allple 12 pro max</h1>
+            <h2>12ram 512 ram</h2>
+            <h2 className='text-red-500'>LKR.12333</h2>
+          </div>
+          
+          <div className='h-9 w-9 object-cover m-2 bg-black bg-opacity-25 flex items-center justify-center rounded-md shadow-shadowOne hover:bg-opacity-40 hover:text-designColor cursor-pointer duration-300'>
+      <MdDeleteForever className='w-6 h-6 hover:text-black text-center text-gray-300 shadow-shadowOne' />
+    </div>
+        </div>
+    </div>
+  )
+}
